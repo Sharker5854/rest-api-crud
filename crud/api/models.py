441d895963acc62e51f374c77e564d98d61bc0f0
Vladimir <kiryanov_vova_134@mail.ru>
2022-09-15 01:58:13 +0300

    Создана модель User.

--- crud/api/models.py
+++ crud/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True, editable=False, verbose_name="User's ID")
+    username = models.CharField(max_length=32, unique=True, verbose_name="Username")
+    password = models.CharField(max_length=255, verbose_name="Password hash")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created account")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated account")
